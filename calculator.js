a34b4364554fee692454ab605a28ce20cc4f73e7
Danilo-Ko <danilosrbija@gmail.com>
2022-12-24 11:02:10 +0100

    인치, 야드 계산식 추가

--- calculator.js
+++ calculator.js
@@ -11,6 +11,18 @@ function inputUnitFunc () {
     }else if(inputUnit.value === "평방미터") {
         inputUnit.value === "평"
         outputUnit.value = "평"
+    }else if(inputUnit.value === "인치") {
+        inputUnit.value === "센티"
+        outputUnit.value = "센티"
+    }else if(inputUnit.value === "센티") {
+        inputUnit.value === "인치"
+        outputUnit.value = "인치"
+    }else if(inputUnit.value === "야드") {
+        inputUnit.value === "미터"
+        outputUnit.value = "미터"
+    }else if(inputUnit.value === "미터") {
+        inputUnit.value === "야드"
+        outputUnit.value = "야드"
     };
 }
 
@@ -19,6 +31,14 @@ function calculatorFunc () {
         outputBox.value = Math.floor(inputBox.value * 3.3);
     }else if (inputUnit.value ==="평방미터") {
         outputBox.value = Math.floor(inputBox.value / 3.3);
+    }else if (inputUnit.value ==="야드") {
+        outputBox.value = Math.floor(inputBox.value *0.9144);
+    }else if (inputUnit.value ==="미터") {
+        outputBox.value = Math.floor(inputBox.value /0.9144);
+    }else if (inputUnit.value ==="인치") {
+        outputBox.value = Math.floor(inputBox.value *2.54);
+    }else if (inputUnit.value ==="센티") {
+        outputBox.value = Math.floor(inputBox.value /2.54);
     }
 }
 
